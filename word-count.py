f7e24fb193457993a34a0d3114482f27fa1328b5
Mohammad Hossein Arab <mohammadhosseinarab110@gmail.com>
2023-09-15 15:41:37 +0330

    make a better algorithm for separate words and know support persian and arabic lang

--- word-count.py
+++ word-count.py
@@ -2,46 +2,40 @@ import docx
 from docx.enum.text import WD_COLOR_INDEX
 
 def HighlightCounter(doc, color):
-    special_chars = ["!", "\"", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", ":", ";", "'", "\\", "|", "~", "`", ",", "<", ".", ">", "/", "?", "،", "؛", ",", "]", "[", "«", "»", ":", "ء", "؟"]
+    words = []
+    words_arrey = []
     words_count = 0
-    for para in doc.paragraphs:
-        for part in para.runs:
-            if part.font.highlight_color ==  color:
-                    words = part.text
-                    words_arrey = words.split(" ")
-                    if words_arrey[0] == "" or words_arrey[-1] == "":
-                        words_arrey.remove("")
-                    if words_arrey[-1] == "":
-                        words_arrey.remove("")
-                    if words_arrey != 0:
-                        for schar in special_chars:
-                            for char in words_arrey:
-                                if schar == char:
-                                    words_arrey.remove(char)
-                    if len(words_arrey) == 0:
-                        words_arrey = 0
-                    if words_arrey != 0:
-                        words_count += len(words_arrey)
+    for paragraph in doc.paragraphs:
+        highlight = ""
+        x = False
+        for run in paragraph.runs:
+            if x == False and highlight != "" and highlight[-1] != " ":
+                highlight += " "
+            if run.font.highlight_color ==  color:
+                highlight += run.text
+                x = True
+            else:
+                x = False
+        if highlight:
+            words.append(highlight)
+    for i in range(len(words)):
+        words_arrey += words[i].split(" ")
+    words_arrey = list(filter(None, words_arrey))
+    print(words_arrey)
+    words_count = len(words_arrey)
     return words_count
 
 def AllWordsCounter(doc):
-    special_chars = ["!", "\"", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", ":", ";", "'", "\\", "|", "~", "`", ",", "<", ".", ">", "/", "?", "،", "؛", ",", "]", "[", "«", "»", ":", "ء", "؟"]
+    words = []
+    words_arrey = []
     words_count = 0
-    for para in doc.paragraphs:
-        for part in para.runs:
-            words = part.text
-            words_arrey = words.split(" ")
-            if words_arrey[0] == "" or words_arrey[-1] == "":
-                words_arrey.remove("")
-            if words_arrey[-1] == "":
-                words_arrey.remove("")
-            if words_arrey != 0:
-                for schar in special_chars:
-                    for char in words_arrey:
-                        if schar == char:
-                            words_arrey.remove(char)
-            if len(words_arrey) == 0:
-                words_arrey = 0
-            if words_arrey != 0:
-                words_count += len(words_arrey)
+    for paragraph in doc.paragraphs:
+        word = ""
+        for run in paragraph.runs:
+            word += run.text
+        if word:
+            words.append(word)
+    for i in range(len(words)):
+        words_arrey += words[i].split(" ")
+    words_count = len(words_arrey)
     return words_count
